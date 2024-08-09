c95cfe7909d3db2fece095ee7850455322f0e8a4
vaibhav <vaibhavpatil8743@gmail.com>
2024-08-09 18:55:33 +0530

    spring data jpa 001 us

---
+++ customerCrudOperation/src/main/java/com/cusotmer/info/customerCrudOperation/CustomerCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.cusotmer.info.customerCrudOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCrudOperationApplication.class, args);
+	}
+
+}
---
+++ customerCrudOperation/src/test/java/com/cusotmer/info/customerCrudOperation/CustomerCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cusotmer.info.customerCrudOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
